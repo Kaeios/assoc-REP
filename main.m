de55d60e77d44addd8d48bf334ac695544fbdf25
Guillaume <kaeios.contact@gmail.com>
2024-10-19 18:57:13 +0200

    Update main.m

--- main.m
+++ main.m
@@ -1,5 +1,6 @@
-pkg load symbolic
-result = assoc(100)
+PAGER('/dev/null')
+pkg load symbolic;
+result = assoc(100);
 fileID = fopen('answer_associativity.txt','w');
 fprintf(fileID, '%f\n', double(result));
 fclose(fileID);
