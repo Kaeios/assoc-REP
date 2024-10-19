6e3331882488d9d3bb4805a471085af806e42a90
Guillaume <kaeios.contact@gmail.com>
2024-10-19 19:34:58 +0200

    Update main.m

--- main.m
+++ main.m
@@ -1,5 +1,9 @@
 PAGER('/dev/null');
-pkg load symbolic;
+more off;  % Ensure no pagination occurs
+
+% Redirect stdout to /dev/null to suppress the message
+evalc('pkg load symbolic');
+
 result = assoc(100);
 fileID = fopen('answer_associativity.txt','w');
 fprintf(fileID, '%f\n', double(result));
