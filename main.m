8962df654495adab46beb29155c007672d7f4203
Guillaume Freyermuth <gfreyerm@insa-rennes.fr>
2024-10-21 08:14:44 +0200

    Set decimal precision to 16

--- main.m
+++ main.m
@@ -4,6 +4,8 @@ more off;  % Ensure no pagination occurs
 % Redirect stdout to /dev/null to suppress the message
 evalc('pkg load symbolic');
 
+digits(16);
+
 result = assoc(100);
 fileID = fopen('answer_associativity.txt','w');
 fprintf(fileID, '%f\n', double(result));
