7cce16147ef9c821820dfbf82ae7ebf634a71bb8
Guillaume Freyermuth <gfreyerm@insa-rennes.fr>
2024-10-21 08:23:40 +0200

    Remove VPA usage

--- main.m
+++ main.m
@@ -2,7 +2,7 @@ PAGER('/dev/null');
 more off;  % Ensure no pagination occurs
 
 % Redirect stdout to /dev/null to suppress the message
-evalc('pkg load symbolic');
+% evalc('pkg load symbolic');
 
 result = assoc(100);
 fileID = fopen('answer_associativity.txt','w');
