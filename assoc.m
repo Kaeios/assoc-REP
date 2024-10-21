f89652a04b29329d19717c26aac21980a7b49239
Guillaume Freyermuth <gfreyerm@insa-rennes.fr>
2024-10-21 08:35:19 +0200

    Up

--- assoc.m
+++ assoc.m
@@ -1,13 +1,14 @@
 function count = assoc(N)
     count = 0;
-    x = rand();
-    y = rand();
-    z = rand();
+
     for i = 1:N
+        x = rand();
+        y = rand();
+        z = rand();
         if (x+y)+z==x+(y+z)
             count = count+1;
         end
     end
 
-    count = count / N;
+    count = count / double(N);
 end
